5f52b59352ed272894d5f81d67385f5b6c450912
chandlerkeyes <ckeyes4@emich.edu>
2017-10-29 20:59:26 -0400

    Created all needed methods for BabyName class

--- com/BabyNames/BabyName.java
+++ com/BabyNames/BabyName.java
@@ -1,8 +1,68 @@
 package com.BabyNames;
 
 public class BabyName {
+    private String name;
+    private boolean gender;
+    private int nameCount;
+    private int birthYear;
 
+    public BabyName(String name, boolean gender, int nameCount, int birthYear) {
+        this.name = name;
+        this.gender = gender;
+        this.nameCount = nameCount;
+        this.birthYear = birthYear;
+    }
+    //if gender is true, then it's a female
+    public boolean isFemale() {
+        if (gender) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNameCount() {
+        return nameCount;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public void setNameCount(int nameCount) {
+        this.nameCount = nameCount;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    //returns formatted string
+    @Override
+    public String toString() {
+        return "BabyName{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", nameCount=" + nameCount +
+                ", birthYear=" + birthYear +
+                '}';
+    }
+
+    //for testing
     public static void main(String[] args) {
-        
+
     }
 }
